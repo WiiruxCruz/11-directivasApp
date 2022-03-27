e65a7554813a28d75fd05d51e775382482aa9818
WiiruxCruz <hialf.92@gmail.com>
2022-03-26 21:28:20 -0600

    Directiva personalizada

--- src/app/productos/productos.module.ts
+++ src/app/productos/productos.module.ts
@@ -4,14 +4,18 @@ import { CommonModule } from '@angular/common';
 import { ProductosRoutingModule } from './productos-routing.module';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [AgregarComponent],
-  imports: [
-    CommonModule,
-    ProductosRoutingModule,
-	ReactiveFormsModule
-  ]
+	declarations: [
+		AgregarComponent
+	],
+	imports: [
+		CommonModule,
+		ProductosRoutingModule,
+		ReactiveFormsModule,
+		SharedModule
+	]
 })
 export class ProductosModule { }
